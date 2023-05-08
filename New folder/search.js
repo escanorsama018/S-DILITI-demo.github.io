14ab2edc32491a5229d878274476bb394a99493b
escanorsama018 <132858614+escanorsama018@users.noreply.github.com>
2023-05-08 04:58:32 +0300

    Update search.js

--- New folder/search.js
+++ New folder/search.js
@@ -2,7 +2,7 @@ function search(){
     let searchbar = document.getElementById('search').value.toLocaleUpperCase();
     let productList = document.querySelector('.itemContainer');
     let product = document.querySelectorAll('.item');
-    let productName = getElementsByTagName('h2');
+    let productName = document.getElementsByTagName('h2');
 
     for (let i = 0 ; i<productName.length ; i++) {
         if (productName[i].innerHTML.toLocaleUpperCase().indexOf(searchbar) >= 0){
